6192fc47bd66f3d2cb9ab94fbbe3cf2f1e357b01
Sant0s-Wesley <wy.santos92@gmail.com>
2023-03-01 00:27:48 +0000

    Aula01

---
+++ Aula01/script.js
@@ -0,0 +1,29 @@
+
+
+
+
+// let nomeompleto = prompt('Digite seu nome completo');
+
+// console.log(nomeompleto);
+
+
+// let numeroEntre0e10 = Math.random() * 10; Math.random() gera numero aleatorios
+
+
+// let meuNome = 'Wesley santos'; // strings entre aspas
+// let meuNumero = 31; // numero solto sem aspas
+
+// console.log(meuNome, meuNumero); //() parâmetros
+
+// let numeroAleatorio = Math.random();
+
+// let a = 10;
+// let b = 2;
+
+// let c = 20;
+
+// let resultado = c * 2;
+
+// console.log(resultado);
+
+// console.log(10 / 2);
